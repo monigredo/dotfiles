f804a38ea69ca2ec754bde087d6de297683e32a6
monig <8849111+monigredo@users.noreply.github.com>
2026-06-04 19:20:31 +0200

    quiet fzf zsh startup warning

--- shell/.config/zsh/55-fzf.zsh
+++ shell/.config/zsh/55-fzf.zsh
@@ -1,3 +1,24 @@
 if [ -r /usr/share/fzf/shell/key-bindings.zsh ]; then
+  zmodload() {
+    return 1
+  }
+
+  setopt() {
+    local -a args
+
+    while (( $# )); do
+      if [[ $1 == [-+]o && ${2-} == zle ]]; then
+        shift 2
+      else
+        args+=("$1")
+        shift
+      fi
+    done
+
+    builtin setopt "$args[@]"
+  }
+
   . /usr/share/fzf/shell/key-bindings.zsh
+
+  unfunction setopt zmodload
 fi
